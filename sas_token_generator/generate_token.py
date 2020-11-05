1d8e6073e308b5245da22805f1c6fc7072e91a61
Suresh Prajapati <er.sureshprajapati@gmail.com>
2020-11-05 13:42:24 +0100

    Update generate_token.py

--- sas_token_generator/generate_token.py
+++ sas_token_generator/generate_token.py
@@ -1,61 +1,42 @@
 import logging
 from datetime import datetime, timedelta
-# import azure.storage.blob
-# from azure.storage.blob import BlobPermissions
-from azure.storage.blob import (
-    BaseBlobService,
-    generate_blob_shared_access_signature,
-    BlobPermissions
-)
+import azure.storage.blob as azureblob
 
+_DEFAULT_SAS_EXPIRY_DAYS = 365 * 30
 
 def parse_conn_string(storage_connection_string):
-    storage_conn = storage_connection_string.split(';')
-    *_, name = storage_conn[1].split('=')
-    *_, key = storage_conn[2].split('=')
-    return name, key
+    conn = storage_connection_string.split(';')
+    for l in conn:
+        ss = l.split('=',1)
+        if len(ss) != 2:
+            continue
+        if ss[0] == 'AccountName':
+           storage_account = ss[1] 
+        if ss[0] == 'AccountKey':
+           storage_key = ss[1]
+    return storage_account, storage_key
 
 def get_token(storage_connection_string,
         container_name,
         blob_name
     ):
-    # sas_token = generate_account_sas(
-    #     account_name="<storage-account-name>",
-    #     account_key="<account-access-key>",
-    #     resource_types=ResourceTypes(service=True),
-    #     permission=AccountSasPermissions(read=True),
-    #     expiry=datetime.utcnow() + timedelta(hours=1)
-    # )
-    storage_account_name , token_credential = parse_conn_string(storage_connection_string)
-    # blob_service_client = baseblobservice.BaseBlobService(connection_string=storage_connection_string)
-    blob_service_client = BaseBlobService(
-        account_url=f'https://{storage_account_name}.blob.core.windows.net',
-        credential=token_credential
+    logging.debug('In generate token function')
+    storage_account_name , storage_account_key = parse_conn_string(storage_connection_string)
+    logging.debug(storage_account_name)
+    blob_service_client = azureblob.BlockBlobService(
+        account_name=storage_account_name,
+        account_key=storage_account_key
     )
-    # sas_token = generate_account_sas(
-    #     storage_account_name,
-    #     storage_account_key,
-    #     resource_types=ResourceTypes(service=True),
-    #     permission=AccountSasPermissions(read=True),
-    #     expiry=datetime.utcnow() + timedelta(hours=1)
-    # )
-
-    sas_signature = blob_service_client.generate_blob_shared_access_signature(
-        container_name,
-        blob_name,
-        permission=BlobPermissions.READ,
-        expiry=datetime.utcnow() + timedelta(hours=1)
-    )
-    # sas_url = blob_service_client.generate_blob_shared_access_signature(
-    #     container,
-    #     blob_name,
-    #     ,
-    #     datetime.now() + timedelta(hours=1)
-    # )
-    # sas_token = generate_account_sas(
-    # account_name=storage_account_name,
-    # account_key=storage_account_key,
-    # resource_types=ResourceTypes(service=True),
-    # permission=AccountSasPermissions(read=True),
+    try:
+        perm = azureblob.BlobPermissions(
+        read=True, create=True, write=True, delete=True)
+        sas_signature = blob_service_client.generate_blob_shared_access_signature(
+            ccontainer_name=container_name,
+            blob_name=blob_name,
+            permission=perm,
+            expiry=datetime.now() + timedelta(days=_DEFAULT_SAS_EXPIRY_DAYS)
+        )
+    except Exception as e:
+        logging.debug(e)
     logging.debug(sas_signature)
     return(f'https://+{storage_account_name}.blob.core.windows.net/{container_name}/{blob_name}?{sas_signature}')
